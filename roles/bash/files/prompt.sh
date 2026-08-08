9a5b63601aaa9a8a3a12fa082e5d4a0b82e76510
Samuel Phan <1516171+samuel-phan@users.noreply.github.com>
2026-08-08 23:19:49 +0200

    fix: remove git info from bash prompt
    It added the Python venv prefix at each command.

--- roles/bash/files/prompt.sh
+++ roles/bash/files/prompt.sh
@@ -1,18 +1,3 @@
-function_exists() {
-    FUNCTION_NAME=$1
-
-    [ -z "$FUNCTION_NAME" ] && return 1
-
-    declare -F "$FUNCTION_NAME" > /dev/null 2>&1
-    return $?
-}
-
-set_title() {
-    ORIG=$PS1
-    TITLE="\[\e]0;$@\a\]"
-    PS1=${ORIG}${TITLE}
-}
-
 bold=$(tput bold)
 red=$(tput setaf 1)
 yellow=$(tput setaf 3)
@@ -27,24 +12,3 @@ fi
 prefix="\[$bold\]\d \t \[$issudo\]\u\[$reset\]@\[$bold$yellow\]\h \[$blue\]\w\[$reset\]"
 suffix="\[$bold$issudo\]\\$\[$reset\]"
 PS1="$prefix $suffix "
-
-# git prompt config
-
-# needed for "sudo su"
-if ! function_exists __git_ps1 && [ -f /etc/bash_completion.d/git-prompt ]; then
-    . /etc/bash_completion.d/git-prompt
-fi
-
-if function_exists __git_ps1; then
-    export GIT_PS1_SHOWCOLORHINTS=1
-    export GIT_PS1_SHOWDIRTYSTATE=1 GIT_PS1_SHOWSTASHSTATE=1 GIT_PS1_SHOWUNTRACKEDFILES=1
-    export GIT_PS1_SHOWUPSTREAM=verbose GIT_PS1_DESCRIBE_STYLE=branch
-
-    export PROMPT_COMMAND='
-    if [[ -n "$VIRTUAL_ENV" ]]; then
-        prefix="($(basename "$VIRTUAL_ENV")) $prefix"
-    fi
-    __git_ps1 "$prefix" " $suffix "
-    set_title "\u@\h: \w"
-    '
-fi
